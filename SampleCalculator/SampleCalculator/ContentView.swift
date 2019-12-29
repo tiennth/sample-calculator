d8e5f4bff6ec869ed633cf3708a2495543ec6839
Tien Nguyen <thanhtien2302@gmail.com>
2019-12-29 11:19:30 +0700

    Add project, update gitignore

---
+++ SampleCalculator/SampleCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleCalculator
+//
+//  Created by Tien on 12/29/19.
+//  Copyright © 2019 Gomu Soft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
